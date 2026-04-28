7c7d73a84152b7eec2e7e01c1d4bad285308cada
Brian <i546779@fontysict.nl>
2026-04-28 12:42:29 +0200

    begin setup

---
+++ main.tf
@@ -0,0 +1,330 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone = var.zone
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region = var.region
+}
+
+# VPC
+
+resource "google_compute_network" "vpc" {
+  name = "platform-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet_gke" {
+  name          = "subnet-gke"
+  ip_cidr_range = "10.10.0.0/20"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  subnetwork = google_compute_subnetwork.subnet_gke.self_link
+}
+
+resource "google_compute_subnetwork" "subnet_db" {
+  name          = "subnet-db"
+  ip_cidr_range = "10.20.0.0/20"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  private_network = google_compute_network.vpc.self_link
+}
+
+
+
+# Firewall regels
+resource "google_compute_firewall" "allow_gke_to_sql" {
+  name    = "allow-gke-to-sql"
+  network = google_compute_network.vpc.name
+
+  direction = "INGRESS"
+  priority  = 1000
+
+  allows {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  source_ranges = ["10.10.0.0/20"]   # GKE subnet var
+  target_tags   = ["cloud-sql"]      # SQL instance tag
+}
+
+resource "google_compute_firewall" "deny_all_to_sql" {
+  name    = "deny-all-to-sql"
+  network = google_compute_network.vpc.name
+
+  direction = "INGRESS"
+  priority  = 2000
+
+  denies {
+    protocol = "all"
+  }
+
+  target_tags = ["cloud-sql"]
+}
+
+resource "google_compute_firewall" "gke_ssh" {
+  name    = "gke-ssh"
+  network = google_compute_network.vpc.name
+
+  direction = "INGRESS"
+  priority  = 1000
+
+  allows {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["YOUR_ADMIN_IP/32"]
+  target_tags   = ["gke-node"]
+}
+
+# GKE cluster
+
+resource "google_container_cluster" "gke" {
+  name     = "platform-gke"
+  location = var.region
+
+  network    = google_compute_network.vpc.self_link
+  subnetwork = google_compute_subnetwork.subnet.self_link
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  ip_allocation_policy {}
+
+  network_policy {
+    enabled  = true
+    provider = "CALICO"
+  }
+
+  workload_identity_config {
+    workload_pool = "YOUR_PROJECT_ID.svc.id.goog"
+  }
+
+  logging_config {
+    enable_components = ["SYSTEM_COMPONENTS", "WORKLOADS"]
+  }
+
+  monitoring_config {
+    enable_components = ["SYSTEM_COMPONENTS", "WORKLOADS"]
+  }
+}
+
+resource "google_container_node_pool" "pool" {
+  name       = "platform-pool"
+  location   = var.region
+  cluster    = google_container_cluster.gke.name
+  node_count = 3
+
+  node_config {
+    machine_type = "e2-standard-4"
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    tags         = ["gke-node"]
+  }
+}
+
+# IAM
+
+resource "google_project_iam_binding" "devs_viewer" {
+  project = "YOUR_PROJECT_ID"
+  role    = "roles/viewer"
+
+  members = [
+    "group:dev-team@example.com"
+  ]
+}
+
+resource "google_project_iam_binding" "platform_admins" {
+  project = "YOUR_PROJECT_ID"
+  role    = "roles/owner"
+
+  members = [
+    "group:platform-admins@example.com"
+  ]
+}
+
+# Logging
+resource "google_bigquery_dataset" "logs" {
+  dataset_id = "platform_logs"
+  project    = "YOUR_PROJECT_ID"
+  location   = "EU"
+}
+
+resource "google_logging_project_sink" "logs_to_bq" {
+  name        = "logs-to-bq"
+  project     = "YOUR_PROJECT_ID"
+  destination = "bigquery.googleapis.com/projects/YOUR_PROJECT_ID/datasets/${google_bigquery_dataset.logs.dataset_id}"
+  filter      = "resource.type=k8s_container OR resource.type=gce_instance"
+
+  unique_writer_identity = true
+}
+
+resource "google_sql_database_instance" "db" {
+  name             = "app-db"
+  database_version = "POSTGRES_15"
+  region           = var.region
+
+  settings {
+    tier = "db-custom-2-7680"
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.vpc.self_link
+      require_ssl = true
+    }
+  }
+}
+
+resource "google_sql_database" "db_default" {
+  name     = "appdb"
+  instance = google_sql_database_instance.db.name
+}
+
+resource "google_storage_bucket" "app_data" {
+  name          = "YOUR_PROJECT_ID-app-data"
+  location      = "EU"
+  storage_class = "STANDARD"
+}
+
+resource "google_storage_bucket" "logs_archive" {
+  name          = "YOUR_PROJECT_ID-logs-archive"
+  location      = "EU"
+  storage_class = "NEARLINE"
+}
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "cluster" {
+  name     = google_container_cluster.gke.name
+  location = var.region
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.cluster.endpoint}"
+  token = data.google_client_config.default.access_token
+
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate
+  )
+}
+
+# Self service portal
+resource "kubernetes_namespace" "platform" {
+  metadata {
+    name = "platform"
+  }
+}
+
+resource "kubernetes_deployment" "portal" {
+  metadata {
+    name      = "self-service-portal"
+    namespace = kubernetes_namespace.platform.metadata[0].name
+    labels = { app = "portal" }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = { app = "portal" }
+    }
+
+    template {
+      metadata { labels = { app = "portal" } }
+
+      spec {
+        container {
+          name  = "portal"
+          image = "europe-west4-docker.pkg.dev/YOUR_PROJECT_ID/platform/portal:latest"
+          port { container_port = 8080 }
+        }
+      }
+    }
+  }
+}
+
+resource "google_service_account" "orchestrator_sa" {
+  account_id   = "orchestrator-sa"
+  display_name = "Orchestrator Service Account"
+}
+
+resource "google_project_iam_binding" "orchestrator_sql" {
+  project = "YOUR_PROJECT_ID"
+  role    = "roles/cloudsql.client"
+
+  members = [
+    "serviceAccount:${google_service_account.orchestrator_sa.email}"
+  ]
+}
+
+resource "kubernetes_deployment" "orchestrator" {
+  metadata {
+    name      = "provisioning-orchestrator"
+    namespace = kubernetes_namespace.platform.metadata[0].name
+    labels = { app = "orchestrator" }
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = { app = "orchestrator" }
+    }
+
+    template {
+      metadata { labels = { app = "orchestrator" } }
+
+      spec {
+        container {
+          name  = "orchestrator"
+          image = "europe-west4-docker.pkg.dev/YOUR_PROJECT_ID/platform/orchestrator:latest"
+
+          env {
+            name  = "PROJECT_ID"
+            value = "YOUR_PROJECT_ID"
+          }
+
+          spec {
+          service_account_name = "orchestrator-sa"
+          }
+
+          port { container_port = 8080 }
+        }
+      }
+    }
+  }
+}
+
+resource "google_iam_workload_identity_pool" "wi_pool" {
+  workload_identity_pool_id = "gke-pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "wi_provider" {
+  workload_identity_pool_id          = google_iam_workload_identity_pool.wi_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "gke-provider"
+  attribute_mapping = {
+    "google.subject" = "assertion.sub"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,32 +1,4 @@
-variable aws_profile {
-  type = string
-}
-
-variable db_name {
-  type = string
-}
-
-variable db_username {
-  type = string
-}
-
-variable db_password {
-  type = string
-}
-
-variable school_ip {
-  type = string
-}
-
-variable mon_ww {
-  type = string
-}
-
-variable mon_acount {
-  type = string
-}
-
-variable mon_port {
+variable project_id {
   type = string
 }
 
@@ -34,66 +6,6 @@ variable region {
   type = string
 }
 
-variable availibility_zone_a {
-  type = string
-}
-
-variable availibility_zone_b {
-  type = string
-}
-
-variable on_prem_lan {
-  type = string
-}
-
-variable on_prem_public_ip {
-  type = string
-}
-
-variable on_prem_public_ip_32 {
-  type = string
-}
-
-variable pre_shared_key_1 {
-  type = string
-}
-
-variable pre_shared_key_2 {
-  type = string
-}
-
-variable "stackstorm_url" {
-  type = string
-}
-
-variable "stackstorm_bootstrap_key" {
-  type = string
-}
-
-variable "stackstorm_password" {
-  type = string
-}
-
-variable "email" {
-  type = string
-}
-
-variable "dh_group" {
-  type = string
-}
-
-variable "encryptie_algoritme" {
-  type = string
-}
-
-variable "integrity_algoritme" {
-  type = string
-}
-
-variable "p1_lifetime" {
-  type = string
-}
-
-variable "p2_lifetime" {
+variable zone {
   type = string
 }
