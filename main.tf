19f7fea58962f527bddc8984766532a2e98deef4
Brian <i546779@fontysict.nl>
2026-05-01 15:43:33 +0200

    test

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "google_container_cluster" "gke" {
   initial_node_count       = 1
 
   ip_allocation_policy {
-    cluster_secondary_range_name  = "gke-pods"
+    cluster_secondary_range_name  = "gke-pods",
     services_secondary_range_name = "gke-services"} 
 
   network_policy {
