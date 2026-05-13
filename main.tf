399b12082c0ea96949503dd7d75f7a09598f512d
Brian <i546779@fontysict.nl>
2026-05-13 10:50:50 +0200

    test

--- main.tf
+++ main.tf
@@ -146,8 +146,7 @@ resource "google_sql_database_instance" "db" {
       private_network = google_compute_network.vpc.self_link
     }
   }
-  gcloud services enable servicenetworking.googleapis.com \
-  --project project-96521d1d-c0a7-4c19-b4c
+ 
   deletion_protection = false
 
   depends_on = [
