25e12f728490f0b38e7924520ef684790015bd0c
Brian <i546779@fontysict.nl>
2026-05-19 10:20:18 +0200

    test

--- main.tf
+++ main.tf
@@ -183,7 +183,7 @@ resource "google_sql_database_instance" "db" {
 
     ip_configuration {
       ipv4_enabled = false
-      private_network = google_compute_network.vpc.self_link
+      private_network = google_compute_subnetwork.subnet_db.self_link
     }
   }
  
